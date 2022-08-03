c1c44463ade22b67f1cdfc70f15d818d09ccc266
TheinNaingOo <scm.theinnaingoo@gmail.com>
2022-08-03 10:23:11 +0630

    #ReactNative-Life-cycle

--- App.tsx
+++ App.tsx
@@ -4,12 +4,16 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-gesture-handler';
 import Home from './src/pages/home';
 import Pan from './src/pages/pan';
+import Parent from './src/pages/lifecycle/Parent';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{flex:1}}>
-      <Pan />
-    </GestureHandlerRootView>
+    // <GestureHandlerRootView style={{flex:1}}>
+    //   <Pan />
+    // </GestureHandlerRootView>
+    <View style={styles.container}>
+      <Parent/>
+    </View>
   );
 }
 
